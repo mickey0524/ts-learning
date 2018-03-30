37c4aada5bac57cb5b26af9ff1039453b1e289f4
mickey0524 <buptbh@163.com>
2018-03-30 17:32:12 +0800

    feat(ts): interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,11 +1,12 @@
 interface StuParam {
   name: string;
   age?: number;
-  readonly mother?: string;
+  readonly mother?: string; // 只读属性
   [propName: string]: any; // 还存在很多其他的变量
 }
 
 function stu(stuParam: StuParam) {
+  // stuParam.mother = 'asdasd';
   console.log(stuParam);
 }
 
@@ -14,6 +15,33 @@ const s = {
   name: 'mickey',
   age: 21,
   school: 'bupt',
+  mother: 'asd',
 };
 
 stu(s);
+
+interface NumberDictionary {
+  [index: string]: string|number;
+}
+
+const obj: NumberDictionary = {
+  name: '白皓',
+};
+
+// 接口之间的继承，同样可以多重继承
+interface Shape {
+  color: string;
+}
+
+interface PenStroke {
+  penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+  sideLength: number;
+}
+
+const square = {} as Square;
+square.color = 'red';
+square.sideLength = 10;
+square.penWidth = 5.0;
